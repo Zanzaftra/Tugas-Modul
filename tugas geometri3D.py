76026eccd7c5d5161ba0a4028f10a29fa2b8a783
Zanzaftra <147015828+Zanzaftra@users.noreply.github.com>
2025-03-05 11:14:03 +0700

    tugas geometri3D.py

--- tugas geometri3D.py
+++ tugas geometri3D.py
@@ -1,17 +1,17 @@
 import math
 
-#mencari volume kubus
+#vol kubus
 def VKubus (s):
     return s * s * s
 
-#mencari volume balok
-def Vbalok (p, l, t):
+#Vol balook
+def VBalok (p, l, t):
     return p * l * t
 
-#mencari volume bola
+#bol Bolaa
 def VBola (r):
     return 4/3 * 3.14 * r**3
 
-#mencari volume prisma
+#Vol Prismaaa
 def VPrisma (LA, t):
     return LA * t
